38eaf950490d99387274a9f5a1df64ebdec8b49c
Jakob Scheumann <jakicoll@1935j.de>
2015-12-30 16:39:56 +0100

    Arguments parsing oder Wie ich einen Tag damit verbrachte, C++ zu lernen und boost zu konfigurieren.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,48 @@
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include <iterator>
 #include <iostream>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 using namespace std;
 
-int main (int argc, char *argv[])
+int main (const int argc, const char* argv[])
 {
-	cout << "Hello world!" << endl;
-	
-	return 0;
+    try {
+        string songpath="asb";
+        string jack_portname;
+        long starttime=123;
+        bool autoconnect=false;
+        po::options_description desc("Allowed options");
+        desc.add_options()
+                ("help", "produce help message and exit")
+                ("starttime", po::value(&starttime)/*->required()*/, "unix timestamp for track synchronisation")
+                ("song", po::value(&songpath)/*->required()*/, "path to the song to play")
+                ("jackport", po::value(&jack_portname), "jack port name")
+                ("speakers", po::value<string>(), "cmplayer connects its own output to the given jack input")
+                ("autoconnect", po::bool_switch(&autoconnect), "cmplayer will automatically connect to speakers");
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            cout << desc << "\n";
+            return 0;
+        }
+
+
+    } catch(exception& e) {
+        cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
+    return 0;
+
 }
 
