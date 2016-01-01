261ef9c6465f89f334d60a47e910acc67ffebaa4
Jakob Scheumann <jakicoll@1935j.de>
2016-01-01 13:54:09 +0100

    --help zeigt Hilfe an. Auch wenn erforderliche Argumente fehlen.

--- main.cpp
+++ main.cpp
@@ -19,20 +19,21 @@ int main (const int argc, const char* argv[])
         po::options_description desc("Allowed options");
         desc.add_options()
                 ("help", "produce help message and exit")
-                ("starttime", po::value(&starttime)/*->required()*/, "unix timestamp for track synchronisation")
-                ("song", po::value(&songpath)/*->required()*/, "path to the song to play")
+                ("starttime", po::value(&starttime)->required(), "unix timestamp for track synchronisation")
+                ("song", po::value(&songpath)->required(), "path to the song to play")
                 ("jackport", po::value(&jack_portname), "jack port name")
                 ("speakers", po::value<string>(), "cmplayer connects its own output to the given jack input")
                 ("autoconnect", po::bool_switch(&autoconnect), "cmplayer will automatically connect to speakers");
 
         po::variables_map vm;
         po::store(po::parse_command_line(argc, argv, desc), vm);
-        po::notify(vm);
 
         if (vm.count("help")) {
             cout << desc << "\n";
             return 0;
         }
+
+        po::notify(vm); //Ensures all required options are given.
 
 
     } catch(exception& e) {
